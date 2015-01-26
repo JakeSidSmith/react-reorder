fbe3e1751e2696afee03166c64bf73aa15999c5b
JakeSidSmith <jake@dabapps.com>
2015-01-26 21:45:37 +0000

    Pass selected item into react-reorderable

--- js/main.js
+++ js/main.js
@@ -61,7 +61,8 @@ window.define(function (require) {
             callback: this.callback,
             listClass: 'my-list',
             itemClass: 'list-item',
-            itemClicked: this.itemClicked}),
+            itemClicked: this.itemClicked,
+            selected: this.state.clickedItem}),
 
           React.createElement('p', null, React.createElement('strong', null, 'Lock vertical')),
           React.createElement('small', null, 'This example has a hold time of 250 milliseconds'),
