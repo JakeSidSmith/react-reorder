e0d6592648283f353c971a74614a98194aaa40eb
Jake 'Sid' Smith <jake@dabapps.com>
2016-12-16 19:03:44 +0000

    Test updating components props

--- tests/mount.test.js
+++ tests/mount.test.js
@@ -8,6 +8,12 @@ describe('mount', function () {
   let wrapper;
 
   class MyComponent extends Component {
+    shouldComponentUpdate () {
+      return true;
+    }
+
+    componentDidUpdate () {}
+
     componentWillMount () {}
 
     componentDidMount () {}
@@ -20,6 +26,8 @@ describe('mount', function () {
   }
 
   const renderSpy = spy(MyComponent.prototype, 'render');
+  const shouldComponentUpdateSpy = spy(MyComponent.prototype, 'shouldComponentUpdate');
+  const componentDidUpdateSpy = spy(MyComponent.prototype, 'componentDidUpdate');
   const componentWillMountSpy = spy(MyComponent.prototype, 'componentWillMount');
   const componentDidMountSpy = spy(MyComponent.prototype, 'componentDidMount');
   const componentWillUnmountSpy = spy(MyComponent.prototype, 'componentWillUnmount');
@@ -38,6 +46,25 @@ describe('mount', function () {
     renderSpy.reset();
     componentWillMountSpy.reset();
     componentDidMountSpy.reset();
+  });
+
+  it('should return and update the component\'s props', function () {
+    const originalProps = wrapper.props();
+
+    expect(originalProps).to.eql({});
+    expect(shouldComponentUpdateSpy).not.to.have.been.called;
+    expect(componentDidUpdateSpy).not.to.have.been.called;
+
+    wrapper.setProps({foo: 'bar'});
+
+    expect(shouldComponentUpdateSpy).to.have.been.called;
+    expect(componentDidUpdateSpy).to.have.been.called;
+
+    const updatedProps = wrapper.props();
+
+    expect(updatedProps).to.eql({foo: 'bar'});
+
+    expect(originalProps).not.to.eql(updatedProps);
   });
 
   it('should unmount a component', function () {
