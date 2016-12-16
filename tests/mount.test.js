1ead6a7b1e8146a5a847ac111a364a31cd0f6115
Jake 'Sid' Smith <jake@dabapps.com>
2016-12-16 18:51:39 +0000

    Begin testing mount util

--- tests/mount.test.js
+++ tests/mount.test.js
@@ -1,1 +1,27 @@
-// Test mount util
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import mount from './helpers/mount';
+import React, { Component } from 'react';
+
+describe('mount', function () {
+
+  // let wrapper;
+
+  class MyComponent extends Component {
+    render () {
+      return <div />;
+    }
+  }
+
+  const renderSpy = spy(MyComponent.prototype, 'render');
+
+  it('should render a component', function () {
+    // wrapper =
+    mount(<MyComponent />);
+
+    expect(renderSpy).to.have.been.called;
+
+    renderSpy.reset();
+  });
+
+});
