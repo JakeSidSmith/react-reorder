25ae1ae36d986c60060a0254d9d2161568d22c58
Jake 'Sid' Smith <jake@dabapps.com>
2016-12-16 00:37:55 +0000

    Rename mount util methods

--- tests/helpers/mount.js
+++ tests/helpers/mount.js
@@ -8,7 +8,7 @@ $.fn.trigger = function (type, data) {
   TestUtils.Simulate[type](this[0], data);
 };
 
-$.fn.tag = function () {
+$.fn.tagName = function () {
   return (this[0].tagName || '').toLowerCase();
 };
 
@@ -30,19 +30,19 @@ function internalMount (component) {
   let instance = ReactDOM.render(component, element);
   let wrapper = $(ReactDOM.findDOMNode(instance));
 
-  defineProperty(wrapper, 'getInstance', function () {
+  defineProperty(wrapper, 'instance', function () {
     return instance;
   });
 
-  defineProperty(wrapper, 'getName', function () {
+  defineProperty(wrapper, 'name', function () {
     return instance.constructor.displayName;
   });
 
-  defineProperty(wrapper, 'getProps', function () {
+  defineProperty(wrapper, 'props', function () {
     return instance.props;
   });
 
-  defineProperty(wrapper, 'getState', function () {
+  defineProperty(wrapper, 'state', function () {
     return instance.state;
   });
 
