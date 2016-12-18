ccb9d6bc3f350789da169328875e732b27201929
Jake 'Sid' Smith <jake@dabapps.com>
2016-12-18 16:29:42 +0000

    Create horizontal children stub

--- tests/helpers/children-stub.js
+++ tests/helpers/children-stub.js
@@ -1,4 +1,7 @@
-export const verticalChildren = []; // [item, placeholder, dragged, item, item]
+// [item, placeholder, dragged, item, item]
+
+export const verticalChildren = [];
+export const horizontalChildren = [];
 
 const itemSize = {
   width: 100,
@@ -28,6 +31,30 @@ for (var i = 0; i < 5; i += 1) {
         bottom: itemSize.top + (itemSize.height * index) + itemSize.height,
         left: itemSize.left,
         right: itemSize.left + itemSize.width,
+        width: itemSize.width,
+        height: itemSize.height
+      };
+    }
+  });
+
+  horizontalChildren.push({
+    getAttribute: function (attr) {
+      if (attr === 'data-placeholder' && index === 1) {
+        return true;
+      }
+
+      if (attr === 'data-dragged' && index === 2) {
+        return true;
+      }
+
+      return false;
+    },
+    getBoundingClientRect: function () {
+      return {
+        top: itemSize.top,
+        bottom: itemSize.top,
+        left: itemSize.left + (itemSize.width * index),
+        right: itemSize.left + (itemSize.width * index) + itemSize.width,
         width: itemSize.width,
         height: itemSize.height
       };
